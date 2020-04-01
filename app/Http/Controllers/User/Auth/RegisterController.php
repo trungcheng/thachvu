4c6910c352220798764c6baca197fc8690876288
Tuan <tuanlao86@gmail.com>
2020-04-01 22:12:15 +0700

    fix send mail

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/account/signin';
+    protected $redirectTo = '/dang-nhap';
 
     /**
      * Create a new controller instance.
